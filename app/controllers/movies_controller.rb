f3b86e7749859e84107ce44210fe4bb0d1c9a8c6
Kevin Moran <g00306474@gmit.ie>
2019-02-24 20:57:19 +0000

    fixed redirect for autograder?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,16 +20,15 @@ class MoviesController < ApplicationController
     session[:ratings] = params[:ratings].keys if params[:ratings]
     session[:sort] = params[:sort] if params[:sort]
     
-    
-    redirect_to movies_path(ratings: Hash[session[:ratings].map {|r| [r,1]}], sort: session[:sort]) if  params[:ratings].nil? || params[:sort].nil?
-    flash.keep
+    if (params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:rating].nil?))
+      flash.keep
+      redirect_to movies_path(ratings: Hash[session[:ratings].map {|r| [r,1]}], sort: session[:sort])
+    end 
     
     @ratings = session[:ratings] || @all_ratings
     @sort = session[:sort] || 'id'
 
     @movies = Movie.where(rating: @ratings).order(@sort)
-    
-    
   end
 
   def new
