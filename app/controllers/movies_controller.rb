ea080f5a2011eed6dad08377836a30b003c5eb37
Kevin Moran <g00306474@gmit.ie>
2019-02-24 20:40:13 +0000

    added flash.keep before redirect

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,12 +20,16 @@ class MoviesController < ApplicationController
     session[:ratings] = params[:ratings].keys if params[:ratings]
     session[:sort] = params[:sort] if params[:sort]
     
-    redirect_to movies_path(ratings: Hash[session[:ratings].map {|r| [r,1]}], sort: session[:sort]) if session[:ratings] && session[:sort] && ( !params[:ratings] || !params[:sort])
-
+    flash.keep
+    redirect_to movies_path(ratings: Hash[session[:ratings].map {|r| [r,1]}], sort: session[:sort]) if  params[:ratings].nil? || params[:sort].nil?
+    
+    
     @ratings = session[:ratings] || @all_ratings
     @sort = session[:sort] || 'id'
 
     @movies = Movie.where(rating: @ratings).order(@sort)
+    
+    
   end
 
   def new
