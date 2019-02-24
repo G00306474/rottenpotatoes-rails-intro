67f02ef3667092f312ec3b0817cfed9567cd14af
Kevin Moran <g00306474@gmit.ie>
2019-02-24 19:39:27 +0000

    added session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,15 +11,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.ratings
-    @selected_ratings = @all_ratings
     
-    @sort_column = params[:sort] || 'id'  # sort using params[:sort]
-    @selected_ratings =  params[:ratings].keys if params[:ratings]
-    @title_hilite = "hilite" if params[:sort] == 'title'
-    @date_hilite = "hilite" if params[:sort] == 'release_date'
+    @all_ratings = Movie.ratings
+    
+    @title_hilite = session[:title_hilite] = "hilite" if params[:sort] == 'title'
+    @date_hilite = session[:date_hilite] = "hilite" if params[:sort] == 'release_date'
 
-    @movies = Movie.where(rating: @selected_ratings).order(@sort_column)
+    redirect_to movies_path(ratings: Hash[session[:ratings].map {|r| [r,1]}], sort: session[:sort]) if session[:ratings] && session[:sort] && ( !params[:ratings] || !params[:sort])
+
+    session[:ratings] = params[:ratings].keys if params[:ratings]
+    session[:sort] = params[:sort] if params[:sort]
+
+    @movies = Movie.where(rating: @ratings).order(@sort)
   end
 
   def new
